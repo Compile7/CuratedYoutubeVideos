f647c668e86a4f0d959170d708e61ffdf053d9ea
abhimanyu singh rathore <abhimanyu.rathore@loginradius.com>
2023-06-10 17:06:21 +0530

    login and playlist

--- lib/main.dart
+++ lib/main.dart
@@ -1,264 +1,298 @@
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Google Sign-In & YouTube Playlist',
+      title: 'Google Login Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Google Sign-In & YouTube Playlist'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
   GoogleSignIn _googleSignIn = GoogleSignIn(
-    scopes: ['email', 'https://www.googleapis.com/auth/youtube'],
+    scopes: [
+      'email',
+      'https://www.googleapis.com/auth/youtube',
+    ],
     clientId:
         "622626307311-2vujec8emo1vp7acu2hcltr3jrk7q36b.apps.googleusercontent.com",
   );
-  bool _isLoggedIn = false;
-  late GoogleSignInAccount _user;
-  List<dynamic> _playlists = [];
-  List<dynamic> _videos = [];
-  String _selectedPlaylistId = '';
-  bool _isPlayerVisible = false;
-  String _selectedVideoUrl = '';
 
-  void _login() async {
-    try {
-      final googleUser = await _googleSignIn.signIn();
-      if (googleUser != null) {
+  GoogleSignInAccount? _currentUser;
+  bool _isLoggedIn = false;
+  List<Map<String, dynamic>> _youtubePlaylist = [];
+  bool _isLoading = false;
+  bool _showPlaylist = false;
+  List<String> _youtubeVideos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _checkIfLoggedIn();
+  }
+
+  Future<void> _checkIfLoggedIn() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? email = prefs.getString('email');
+
+    if (email != null) {
+      setState(() {
+        _isLoggedIn = true;
+      });
+      _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
         setState(() {
-          _isLoggedIn = true;
-          _user = googleUser;
+          _currentUser = account;
         });
-        _fetchPlaylists();
-      }
-    } catch (error) {
-      print('Error logging in: $error');
+      });
+      await _googleSignIn.signInSilently();
+
+      _fetchYouTubePlaylist(); // Fetch playlist if already logged in
     }
   }
 
-  void _logout() async {
+  Future<void> _handleSignIn() async {
     try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser != null) {
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString('email', googleUser.email);
+        setState(() {
+          _isLoggedIn = true;
+          _currentUser = googleUser;
+        });
+
+        _fetchYouTubePlaylist(); // Fetch playlist after successful login
+      }
+    } catch (error) {
+      print('Error signing in: $error');
+    }
+  }
+
+  Future<void> _handleSignOut() async {
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.remove('email');
+
+      await _googleSignIn.disconnect();
       await _googleSignIn.signOut();
       setState(() {
         _isLoggedIn = false;
-        _playlists.clear();
-        _videos.clear();
-        _selectedPlaylistId = '';
-        _isPlayerVisible = false;
-        _selectedVideoUrl = '';
+        _currentUser = null;
+        _youtubePlaylist = [];
+        _youtubeVideos = [];
       });
     } catch (error) {
-      print('Error logging out: $error');
+      print('Error signing out: $error');
     }
   }
 
-  Future<void> _fetchPlaylists() async {
+  Future<void> _fetchYouTubePlaylist() async {
+    setState(() {
+      _isLoading = true;
+    });
+
     try {
-      final accessToken = await _googleSignIn.currentUser!.authHeaders;
+      final headers = await _googleSignIn.currentUser?.authHeaders;
+      if (headers == null) {
+        throw Exception('User is not authenticated.');
+      }
       final response = await http.get(
         Uri.parse(
-          'https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true',
-        ),
-        headers: accessToken,
+            'https://www.googleapis.com/youtube/v3/playlists?part=snippet&mine=true'),
+        headers: {
+          'Authorization': '${headers["Authorization"]}',
+        },
       );
 
       if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
+        final data = json.decode(response.body);
+        final items = data['items'] as List<dynamic>;
         setState(() {
-          _playlists = data['items'];
+          _youtubePlaylist = items
+              .map<Map<String, dynamic>>((item) => {
+                    'title': item['snippet']['title'],
+                    'id': item['id'],
+                  })
+              .toList();
+          _isLoading = false;
         });
       } else {
-        print('Failed to load playlists. Status code: ${response.statusCode}');
+        print(
+            'Error fetching YouTube playlist. Status code: ${response.statusCode}');
+        setState(() {
+          _isLoading = false;
+        });
       }
     } catch (error) {
-      print('Error fetching playlists: $error');
+      print('Error fetching YouTube playlist: $error');
+      setState(() {
+        _isLoading = false;
+      });
     }
   }
 
-  Future<void> _fetchVideos(String playlistId) async {
+  Future<void> _fetchRelatedVideos(String playlistId) async {
+    setState(() {
+      _isLoading = true;
+    });
     try {
-      final accessToken = await _googleSignIn.currentUser!.authHeaders;
+      final headers = await _googleSignIn.currentUser?.authHeaders;
+      if (headers == null) {
+        throw Exception('User is not authenticated.');
+      }
       final response = await http.get(
         Uri.parse(
-          'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=$playlistId',
-        ),
-        headers: accessToken,
+            'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=$playlistId'),
+        headers: {
+          'Authorization': '${headers["Authorization"]}',
+        },
       );
 
       if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
+        final data = json.decode(response.body);
+        final items = data['items'] as List<dynamic>;
+
         setState(() {
-          _videos = data['items'];
+          _youtubeVideos =
+              items.map<String>((item) => item['snippet']['title']).toList();
+          _isLoading = false;
         });
       } else {
-        print('Failed to load videos. Status code: ${response.statusCode}');
+        print(
+            'Error fetching related videos. Status code: ${response.statusCode}');
+        setState(() {
+          _isLoading = false;
+        });
       }
     } catch (error) {
-      print('Error fetching videos: $error');
+      print('Error fetching related videos: $error');
+      setState(() {
+        _isLoading = false;
+      });
     }
-  }
-
-  void _playVideo(String videoUrl) {
-    setState(() {
-      _isPlayerVisible = true;
-      _selectedVideoUrl = videoUrl;
-    });
-  }
-
-  void _closePlayer() {
-    setState(() {
-      _isPlayerVisible = false;
-      _selectedVideoUrl = '';
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        actions: <Widget>[
-          if (_isLoggedIn)
-            Row(
-              children: <Widget>[
-                Text(
-                  _user.displayName ?? '',
-                  style: TextStyle(fontSize: 16),
-                ),
-                SizedBox(width: 8),
-                IconButton(
-                  icon: Icon(Icons.account_circle),
-                  onPressed: () {
-                    setState(() {
-                      _isLoggedIn = !_isLoggedIn;
-                      _playlists.clear();
-                      _videos.clear();
-                      _selectedPlaylistId = '';
-                    });
-                  },
-                ),
-              ],
-            ),
-        ],
+        title: Text('Google Login Demo'),
       ),
-      body: Row(
-        children: <Widget>[
-          AnimatedContainer(
-            duration: Duration(milliseconds: 500),
-            width: _isPlayerVisible ? 0 : 200,
-            child: ListView.builder(
-              itemCount: _playlists.length,
-              itemBuilder: (context, index) {
-                final playlist = _playlists[index];
-                return ListTile(
-                  title: Text(playlist['snippet']['title']),
-                  onTap: () {
-                    final playlistId = playlist['id'];
-                    setState(() {
-                      _selectedPlaylistId = playlistId;
-                    });
-                    _fetchVideos(playlistId);
-                  },
-                );
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(_currentUser?.photoUrl ?? ''),
+                    radius: 30,
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    _currentUser?.displayName ?? '',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                  Text(
+                    _currentUser?.email ?? '',
+                    style: TextStyle(fontSize: 12, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: Text('Playlist'),
+              leading: Icon(Icons.playlist_play),
+              onTap: () {
+                setState(() {
+                  _showPlaylist = !_showPlaylist;
+                });
               },
             ),
-          ),
-          Expanded(
-            child: _isPlayerVisible
-                ? Column(
-                    children: [
-                      Container(
-                        height: 300,
-                        child: YoutubePlayer(
-                          controller: YoutubePlayerController(
-                            initialVideoId: YoutubePlayer.convertUrlToId(
-                                    _selectedVideoUrl) ??
-                                '',
-                            flags: YoutubePlayerFlags(
-                              autoPlay: true,
-                            ),
-                          ),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: _closePlayer,
-                        child: Text('Back'),
-                      ),
-                    ],
-                  )
-                : GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 1,
-                      childAspectRatio: 16 / 9,
-                    ),
-                    itemCount: _videos.length,
-                    itemBuilder: (context, index) {
-                      final video = _videos[index];
-                      final snippet = video['snippet'];
-                      final title = snippet['title'];
-                      final thumbnail = snippet['thumbnails']['medium']['url'];
-                      final videoId = video['id'];
-                      final videoUrl =
-                          'https://www.youtube.com/watch?v=$videoId';
-
-                      return GestureDetector(
-                        onTap: () => _playVideo(videoUrl),
-                        child: Card(
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: Image.network(
-                                  thumbnail,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  title,
-                                  style: TextStyle(fontSize: 16),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-          ),
-        ],
-      ),
-      floatingActionButton: _isLoggedIn
-          ? FloatingActionButton(
-              onPressed: _logout,
-              child: Icon(Icons.logout),
-            )
-          : FloatingActionButton(
-              onPressed: _login,
-              child: Icon(Icons.login),
+            if (_showPlaylist) ..._buildPlaylistItems(),
+            ListTile(
+              title: Text('Sign Out'),
+              leading: Icon(Icons.logout),
+              onTap: _handleSignOut,
             ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (!_isLoggedIn)
+              ElevatedButton(
+                onPressed: _handleSignIn,
+                child: Text('Sign in with Google'),
+              )
+            else if (_isLoading)
+              CircularProgressIndicator()
+            else if (_youtubeVideos.isNotEmpty)
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: MediaQuery.of(context).size.width < 600
+                      ? 1
+                      : MediaQuery.of(context).size.width < 1200
+                          ? 3
+                          : 4,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  padding: EdgeInsets.all(10),
+                  children: _youtubeVideos.map((videoTitle) {
+                    return GridTile(
+                      child: Container(
+                        color: Colors.grey[300],
+                        child: Center(
+                          child: Text(
+                            videoTitle,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
+  }
+
+  List<Widget> _buildPlaylistItems() {
+    return _youtubePlaylist
+            .map((playlistData) => ListTile(
+                  title:  Text(playlistData['title']),
+                  enabled: !_isLoading,
+                  onTap: () {
+                    _fetchRelatedVideos(playlistData['id']);
+                  },
+                ))
+            .toList();
   }
 }
